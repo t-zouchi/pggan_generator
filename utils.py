dc57aff1ec5de508c3daf3286b0e93cd55434eae
t-zouchi <mojamoja.000@gmail.com>
2018-12-29 03:11:34 +0900

    generate gif

--- utils.py
+++ utils.py
@@ -10,3 +10,18 @@ def save_image(img, filename):
     img = np.uint8(img)
     img = Image.fromarray(img)
     img.save(filename)
+
+def save_imges_to_gif(images, filename, _duration):
+    _images = []
+    
+    for img in images:
+        img = np.transpose(img, (1, 2, 0))
+        img = img * 256
+        img = img.astype(np.int32)
+        img[img < 0] = 0
+        img[img >= 256] = 255
+        img = np.uint8(img)
+        img = Image.fromarray(img)
+        _images.append(img)
+        
+    _images[0].save(filename,save_all=True, append_images=_images[1:], optimize=False, duration=_duration, loop=0)    
